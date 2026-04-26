c860db49a06afbf887ee7809b654b8a6cae336c8
Mei Mei <seraphicfae@proton.me>
2026-04-25 21:26:38 -0400

    chore: removed old git history

---
+++ README.md
@@ -0,0 +1,10 @@
+## [My website](https://seraphicfae.dev)
+
+## Getting started
+
+```sh
+git clone https://github.com/seraphicfae/seraphicfae.dev
+cd seraphicfae.dev
+pnpm install
+pnpm run dev
+```
---
+++ src/content.config.ts
@@ -0,0 +1,30 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const projects = defineCollection({
+	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			date: z.coerce.date(),
+			tags: z.array(z.string()).optional(),
+			github: z.url().optional(),
+			heroImage: image().optional(),
+			heroImageCredit: z.string().optional(),
+			heroImageCreditUrl: z.url().optional(),
+		}),
+});
+
+const blog = defineCollection({
+	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+		tags: z.array(z.string()).optional(),
+	}),
+});
+
+export const collections = { projects, blog };
