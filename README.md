0b91fa906d8cd7e6830e8ef8944588c05e5ecb14
Mei Mei <seraphicfae@proton.me>
2026-05-31 07:15:05 -0400

    chore: clean up old git history

---
+++ README.md
@@ -0,0 +1,13 @@
+## [My website](https://seraphicfae.dev)
+
+Opengraph image [source](https://www.pixiv.net/en/artworks/99003671) \
+Favicon SVG [source](https://codeberg.org/tastytea/blobcats)
+
+## Getting started
+
+```sh
+git clone https://github.com/seraphicfae/seraphicfae.dev
+cd seraphicfae.dev
+pnpm install
+pnpm run dev
+```
---
+++ src/content.config.ts
@@ -0,0 +1,28 @@
+import { defineCollection, type SchemaContext } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const projects = defineCollection({
+	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
+	schema: ({ image }: SchemaContext) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			date: z.coerce.date(),
+			github: z.url().optional(),
+			heroImage: image().optional(),
+			heroImageCredit: z.string().optional(),
+			heroImageCreditUrl: z.url().optional(),
+		}),
+});
+
+const blog = defineCollection({
+	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+	}),
+});
+
+export const collections = { projects, blog };
